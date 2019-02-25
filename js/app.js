ff243845bd28b6b797946742b10ebfb13b12e53e
David Hachuel <david@auggi.ai>
2019-02-25 17:29:01 -0500

    New playground FHIR.

---
+++ js/app.js
@@ -0,0 +1,60 @@
+// function getPatientName (pt) {
+//   if (pt.name) {
+//     var names = pt.name.map(function(name) {
+//       return name.given.join(" ") + " " + name.family.join(" ");
+//     });
+//     return names.join(" / ")
+//   } else {
+//     return "anonymous";
+//   }
+// }
+//
+// function getMedicationName (medCodings) {
+//   var coding = medCodings.find(function(c){
+//     return c.system == "http://www.nlm.nih.gov/research/umls/rxnorm";
+//   });
+//
+//   return coding && coding.display || "Unnamed Medication(TM)"
+// }
+//
+// function displayPatient (pt) {
+//   document.getElementById('patient_name').innerHTML = getPatientName(pt);
+// }
+//
+// var med_list = document.getElementById('med_list');
+
+function displayMedication (medCodings) {
+  med_list.innerHTML += "<li> " + getMedicationName(medCodings) + "</li>";
+}
+
+// Create a FHIR client (server URL, patient id in `demo`)
+var smart = FHIR.client(demo);
+    // pt = smart.patient;
+
+document.getElementById('title').innerHTML = "Welcome to the GI on FHIR app!";
+
+
+smart.api.search({
+  type: "Condition",
+  query: {code: "14760008"}
+}).then(function(r){
+   console.log(JSON.stringify(r, null, 2));
+});
+
+
+// // Create a patient banner by fetching + rendering demographics
+// smart.patient.read().then(function(pt) {
+//   displayPatient (pt);
+// });
+//
+// // A more advanced query: search for active Prescriptions, including med details
+// smart.patient.api.fetchAllWithReferences({type: "MedicationOrder"},["MedicationOrder.medicationReference"]).then(function(results, refs) {
+//    results.forEach(function(prescription){
+//         if (prescription.medicationCodeableConcept) {
+//             displayMedication(prescription.medicationCodeableConcept.coding);
+//         } else if (prescription.medicationReference) {
+//             var med = refs(prescription, prescription.medicationReference);
+//             displayMedication(med && med.code.coding || []);
+//         }
+//    });
+// });
---
+++ js/config.js
@@ -0,0 +1,13 @@
+var demo = {
+  serviceUrl: "https://r3.smarthealthit.org",
+  auth: {
+    type: 'none'
+  }
+};
+
+/*
+var demo = {
+  serviceUrl: "http://fhir.healthintersections.com.au/open",
+  patientId: "smart-880378"
+};
+*/
