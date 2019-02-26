192856e3a9c9b1ed4112e5e6467111b4b7934d29
David Hachuel <david@auggi.ai>
2019-02-25 23:10:44 -0500

    Integrated FHIR scripts with front-end.

--- js/app.js
+++ js/app.js
@@ -1,70 +1,65 @@
-// function getPatientName (pt) {
-//   if (pt.name) {
-//     var names = pt.name.map(function(name) {
-//       return name.given.join(" ") + " " + name.family.join(" ");
-//     });
-//     return names.join(" / ")
-//   } else {
-//     return "anonymous";
-//   }
-// }
-//
-// function getMedicationName (medCodings) {
-//   var coding = medCodings.find(function(c){
-//     return c.system == "http://www.nlm.nih.gov/research/umls/rxnorm";
-//   });
-//
-//   return coding && coding.display || "Unnamed Medication(TM)"
-// }
-//
-// function displayPatient (pt) {
-//   document.getElementById('patient_name').innerHTML = getPatientName(pt);
-// }
-//
-
 // Create a FHIR client (server URL, patient id in `demo`)
 var smart = FHIR.client(config);
 
-document.getElementById('title').innerHTML = "Welcome to the GI on FHIR app!";
 
+function fetchComorbidities(){
+  $("#table-data").empty();
+  var selected_conditions = [];
 
-smart.api.search({
-  type: "Condition",
-  query: {code : {$or : ["14760008", "62315008", "21522001"]}}
-}).then(function(r){
-  var patients = [];
-  for (var i in r.data.entry){
-    patients.push(r.data.entry[i].resource.subject.reference.split("/")[1]);
-    // console.log(r.data.entry[i].resource.subject.reference.split("/")[1]);
+  if(document.getElementById("abdominal-pain").checked){
+    selected_conditions.push("21522001");
   }
-  console.log(patients);
-   // console.log(JSON.stringify(r.data.entry, null, 2));
+  if(document.getElementById("diarrhea").checked){
+    selected_conditions.push("62315008");
+  }
+  if(document.getElementById("constipation").checked){
+    selected_conditions.push("14760008");
+  }
 
-   smart.api.search({
-     type: "Condition",
-     query: {'subject' : {$or : patients}}
-   }).then(function(r){
-     for (var i in r.data.entry){
-       console.log(r.data.entry[i].resource.code.coding[0].display);
-     }
+  console.log(selected_conditions);
+
+  smart.api.search({
+    type: "Condition",
+    query: {code : {$or : selected_conditions}}
+  }).then(function(r){
+    var patients = [];
+    for (var i in r.data.entry){
+      patients.push(r.data.entry[i].resource.subject.reference.split("/")[1]);
+      // console.log(r.data.entry[i].resource.subject.reference.split("/")[1]);
+    }
+    console.log(patients);
      // console.log(JSON.stringify(r.data.entry, null, 2));
-   });
-});
 
+     smart.api.search({
+       type: "Condition",
+       query: {'subject' : {$or : patients}}
+     }).then(function(r){
+       var conditions = {};
+       for (var i in r.data.entry){
+         var condition_code = r.data.entry[i].resource.code.coding[0].code;
+         var condition_name = r.data.entry[i].resource.code.coding[0].display;
+         console.log(condition_code);
+         if (condition_code in conditions){
+           conditions[condition_code]["count"] += 1;
+         } else {
+           conditions[condition_code] = {
+             "count" : 1,
+             "name" : condition_name
+           };
+         }
+       }
+       console.log(JSON.stringify(conditions, null, 2));
+       for(var key in conditions){
+         $("#table-data").append(
+           '<tr>'+
+               '<td>'+conditions[key]["name"]+'</td>'+
+               '<td>'+key+'</td>'+
+               '<td>'+conditions[key]["count"]+'</td>'+
+           '</tr>'
+         )
+       }
+     });
+  });
+}
 
-// // Create a patient banner by fetching + rendering demographics
-// smart.patient.read().then(function(pt) {
-//   displayPatient (pt);
-// });
-//
-// // A more advanced query: search for active Prescriptions, including med details
-// smart.patient.api.fetchAllWithReferences({type: "MedicationOrder"},["MedicationOrder.medicationReference"]).then(function(results, refs) {
-//    results.forEach(function(prescription){
-//         if (prescription.medicationCodeableConcept) {
-//             displayMedication(prescription.medicationCodeableConcept.coding);
-//         } else if (prescription.medicationReference) {
-//             var med = refs(prescription, prescription.medicationReference);
-//             displayMedication(med && med.code.coding || []);
-//         }
-//    });
-// });
+// var conditions = fetchComorbidities(["14760008", "62315008", "21522001"])
